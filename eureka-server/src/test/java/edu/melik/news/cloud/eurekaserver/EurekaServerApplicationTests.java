ae79e5aaa17e4406852440542aa6c0d513a6b260
Aram Meliksetyan <aram.meliksetyan@sibers.com>
2023-03-01 12:54:01 +0600

    added eureka-server

---
+++ eureka-server/src/test/java/edu/melik/news/cloud/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+//package edu.melik.news.cloud.eurekaserver;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EurekaServerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
