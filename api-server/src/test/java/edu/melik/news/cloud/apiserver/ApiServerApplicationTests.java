7c29b1d45e58b6ca379dfdf2c9aa9b0575457649
Aram Meliksetyan <aram.meliksetyan@sibers.com>
2023-03-01 12:54:59 +0600

    added api-server

---
+++ api-server/src/test/java/edu/melik/news/cloud/apiserver/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+//package edu.melik.news.cloud.apiserver;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApiServerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
