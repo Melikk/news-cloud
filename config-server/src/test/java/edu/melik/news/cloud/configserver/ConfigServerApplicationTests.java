511dc555e57851eaae5187f12daa6576f801a64d
Aram Meliksetyan <aram.meliksetyan@sibers.com>
2023-03-01 12:56:11 +0600

    added config-server

---
+++ config-server/src/test/java/edu/melik/news/cloud/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+//package edu.melik.news.cloud.configserver;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ConfigServerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
