0b3c12a4b40c39c98642d4a3a63ecfa5d8608b7b
Aram Meliksetyan <aram.meliksetyan@sibers.com>
2023-03-01 12:55:27 +0600

    added api-gateway

---
+++ api-gateway/src/test/java/edu/melik/news/cloud/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.melik.news.cloud.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
