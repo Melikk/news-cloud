303d1de51eb786994628c470162583372e641f89
Aram Meliksetyan <aram.meliksetyan@sibers.com>
2023-03-01 14:33:26 +0600

    properties moved to config-server

--- api-gateway/src/test/java/edu/melik/news/cloud/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/edu/melik/news/cloud/apigateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,13 @@
-package edu.melik.news.cloud.apigateway;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiGatewayApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package edu.melik.news.cloud.apigateway;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApiGatewayApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
